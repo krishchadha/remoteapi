db23b997d83f2336efeb98e01ce789ef41790562
Krish Chadha <krishchadha94@gmail.com>
2024-10-12 23:56:22 +0530

    1

---
+++ krishapp.py
@@ -0,0 +1,54 @@
+import requests
+import boto3
+from datetime import datetime
+
+sns_client = boto3.client('sns', region_name='ap-south-1')
+s3_client = boto3.client('s3')
+API_URL = 'https://jsonplaceholder.typicode.com/posts'
+SNS_TOPIC_ARN = "arn:aws:sns:ap-south-1:980921721207:krish-sns-topic"  
+S3_BUCKET_NAME = "krish-logs-assignment" 
+
+def fetch_messages_from_api():
+    response = requests.get(API_URL)
+    if response.status_code == 200:
+        print("Sucess")
+        return response.json()
+    else:
+        print(f"Failed with Status Code: {response.status_code}")
+        return []
+        
+
+def publish_message_to_sns(message):
+    try:
+        response = sns_client.publish(
+            TopicArn=SNS_TOPIC_ARN,
+            Message=str(message)
+        )
+        print(f"Message to SNS: {response['MessageId']}")
+    except Exception as e:
+        print(f"Failed SNS")
+
+
+def log_message_to_s3(message):
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S_%f")
+    log_file_name = f'logs/log_{timestamp}.txt'
+    
+    try:
+        s3_client.put_object(
+            Bucket=S3_BUCKET_NAME,
+            Key=log_file_name,
+            Body=str(message)
+        )
+        print("saved S3")
+    except Exception as e:
+        print("Failed S3")
+
+
+def main():
+    messages = fetch_messages_from_api()
+    for message in messages:
+        publish_message_to_sns(message)
+        log_message_to_s3(message)
+
+if __name__ == "__main__":
+    main()
